395b1b20712a3856c443fec067f5ec8fe878e6c2
p r o c e s s o r <59508966+Proc3ssa@users.noreply.github.com>
2023-02-14 19:58:43 +0000

    Add files via upload
    first commit 😊👌

---
+++ welcome/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.timee = New System.Windows.Forms.Label()
+        Me.datee = New System.Windows.Forms.Label()
+        Me.pbar = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(4, 4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(397, 125)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.ProgressBar1)
+        Me.Panel2.Controls.Add(Me.timee)
+        Me.Panel2.Controls.Add(Me.datee)
+        Me.Panel2.Controls.Add(Me.pbar)
+        Me.Panel2.Location = New System.Drawing.Point(2, 3)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(393, 119)
+        Me.Panel2.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Cyan
+        Me.Label1.Font = New System.Drawing.Font("Stencil", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(326, 4)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 14)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "F a i s a l"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Cyan
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI Semibold", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Location = New System.Drawing.Point(322, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(67, 20)
+        Me.Button2.TabIndex = 5
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 41)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(156, 51)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.Button1.Font = New System.Drawing.Font("Centaur", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.InactiveBorder
+        Me.Button1.Location = New System.Drawing.Point(189, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 78)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "WELCOME SIR..."
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(6, 22)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(2, 90)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'timee
+        '
+        Me.timee.AutoSize = True
+        Me.timee.ForeColor = System.Drawing.Color.Cyan
+        Me.timee.Location = New System.Drawing.Point(72, 3)
+        Me.timee.Name = "timee"
+        Me.timee.Size = New System.Drawing.Size(41, 15)
+        Me.timee.TabIndex = 1
+        Me.timee.Text = "Label1"
+        '
+        'datee
+        '
+        Me.datee.AutoSize = True
+        Me.datee.Location = New System.Drawing.Point(3, 3)
+        Me.datee.Name = "datee"
+        Me.datee.Size = New System.Drawing.Size(41, 15)
+        Me.datee.TabIndex = 1
+        Me.datee.Text = "Label1"
+        '
+        'pbar
+        '
+        Me.pbar.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.pbar.ForeColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.pbar.Location = New System.Drawing.Point(6, 22)
+        Me.pbar.Name = "pbar"
+        Me.pbar.Size = New System.Drawing.Size(382, 2)
+        Me.pbar.Step = 5
+        Me.pbar.TabIndex = 0
+        '
+        'Timer1
+        '
+        '
+        'form1
+        '
+        Me.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(405, 133)
+        Me.Controls.Add(Me.Panel1)
+        Me.Cursor = System.Windows.Forms.Cursors.PanNW
+        Me.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "form1"
+        Me.Opacity = 0.95R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "welcome"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents pbar As ProgressBar
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents datee As Label
+    Friend WithEvents timee As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ welcome/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class form1
+    Private Sub first_Load(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        datee.Text = Date.Now.ToString("dd-mm-yyyy")
+        timee.Text = Date.Now.ToString("hh:mm:ss")
+
+    End Sub
+
+    Private Sub form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+        My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Asterisk)
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
